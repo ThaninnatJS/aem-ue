22e20c06ed0d07b426302fc624c310477915edd6
adls <student@example.com>
2026-05-20 07:38:27 +0000

    change footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -11,7 +11,7 @@ export default async function decorate(block) {
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
-  // decorate footer DOM
+  // decorate footer DOM TEST
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
